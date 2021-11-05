23fd4ff19f78dc82ca7af896ffb03821f5f7b366
Juliana Bearzi <julietad299@gmail.com>
2021-11-05 08:31:09 -0300

    add thunk configuration

--- src/index.jsx
+++ src/index.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { composeWithDevTools } from 'redux-devtools-extension';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
 import rootReducer from './redux/reducers/rootReducer';
 import './index.css';
 import App from './App';
 
 const configureStore = () => {
-  const enhancer = composeWithDevTools();
+  const enhancer = composeWithDevTools(applyMiddleware(thunk));
   return createStore(rootReducer, enhancer);
 };
 
